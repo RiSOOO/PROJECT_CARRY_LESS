7d26baaf60790c03a03bd602775637a5a25e6e96
ssslipknottt <160328427+ssslipknottt@users.noreply.github.com>
2024-09-17 10:20:56 +0200

    dalsi nastavení zobrazeni Kategorie a produkty jenom se jmenem a IDčem na main.html

--- Carry_less/viewer/views.py
+++ Carry_less/viewer/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from viewer.models import Categorie
+from viewer.models import Product
 
 class MainPageView(TemplateView):
     template_name = 'main.html'
     extra_context = {
-    "all_categories": Categorie.objects.all()
+    "all_categories": Categorie.objects.all(),
+    "all_products": Product.objects.all()
 
     }
 
