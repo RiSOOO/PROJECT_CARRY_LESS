1b476490c213565f1dba93854772184ec5472f80
philosoraptorka <philosoraptorka@gmail.com>
2024-09-22 10:27:54 +0200

    optimalizace počtu znaků (CharField) v modelech v models.py

--- Carry_less/viewer/models.py
+++ Carry_less/viewer/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Categorie(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=70)
     def __str__(self):
         return f"Kategorie {self.name}"
 
 class Product(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=70)
     weight = models.IntegerField()
     price = models.IntegerField()
-    brand = models.CharField(max_length=300)
+    brand = models.CharField(max_length=70)
     category_ID = models.IntegerField()
     products_of_categories = models.ForeignKey(Categorie, on_delete=models.DO_NOTHING, null=True)
 
