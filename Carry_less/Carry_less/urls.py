fa51fcbd12b6621e3492f015d7a092383b17aade
ssslipknottt <160328427+ssslipknottt@users.noreply.github.com>
2024-10-01 09:46:53 +0200

    feat: funkcni login tlacidlo a log out tlacidlo.

--- Carry_less/Carry_less/urls.py
+++ Carry_less/Carry_less/urls.py
@@ -5,13 +5,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.Home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -22,6 +22,7 @@ from viewer.models import Product
 from viewer.models import ProductFeature
 
 from django.contrib.auth.views import LogoutView, LoginView
+
 
 admin.site.register(Categorie)
 admin.site.register(Product)
@@ -41,7 +42,7 @@ urlpatterns = [
     path('produkty/update/<int:pk>/', ProductsUpdateView.as_view(), name='update_product'),
     path('produkty/delete/<int:pk>/', ProductsDeleteView.as_view(), name='delete_product'),
 
-    path('users/login/', LogoinView.as_view(), name='login'),
+    path('users/login/', LoginView.as_view(), name='login'),
     path('users/logout/', LogoutView.as_view(), name='logout')
 
 
