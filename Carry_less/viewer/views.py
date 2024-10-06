a0debff885cf7a184abce22884a92dca8639870a
AlbertA <albert.mysak@gmail.com>
2024-10-06 15:06:46 +0200

    feat: kategorie produkty

--- Carry_less/viewer/views.py
+++ Carry_less/viewer/views.py
@@ -26,7 +26,8 @@ class CategoriesView(TemplateView):
             }
         else:
             extra_context={
-                "category": Categorie.objects.get(id=category_id)
+                "category": Categorie.objects.get(id=category_id),
+                "products": Product.objects.filter(products_of_categories=category_id)
             }
         return render(request,"kategorie.html",extra_context)
 
