88e51b85f69f125d291d77981086744094a2dfe2
AlbertA <albert.mysak@gmail.com>
2024-10-12 10:50:32 +0200

    feat: validace

--- Carry_less/viewer/forms.py
+++ Carry_less/viewer/forms.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 from viewer.models import Product
 
@@ -5,6 +6,18 @@ class ProductsForm(ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean_name(self):
+        if len(self.cleaned_data["name"]) < 3:
+            raise ValidationError("název je moc krátký")
+        if Product.objects.filter(name=self.cleaned_data["name"]).exists():
+            raise ValidationError("produkt s tímto názvem již existuje")
+        return self.cleaned_data["name"]
+
+    def clean_price(self):
+        if self.cleaned_data["price"] > 0:
+            return self.cleaned_data["price"]
+        raise ValidationError("cena musí být větší než nula")
 
 
 
