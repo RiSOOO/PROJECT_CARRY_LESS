48481c00853cf3c597595ba4ddef7e5268680675
ssslipknottt <160328427+ssslipknottt@users.noreply.github.com>
2024-10-18 14:28:06 +0200

    feat: prvni test nefunkcni nevim proc

--- Carry_less/viewer/tests.py
+++ Carry_less/viewer/tests.py
@@ -1,3 +1,14 @@
+from django.urls import reverse
 from django.test import TestCase
+from viewer.models import Product
 
-# Create your tests here.
+
+class ProductTest(TestCase):
+    def setUp(self):
+        # Vytvoření testovacího produktu
+        Product.objects.create(name="Test product", price=10)
+
+    def test_product(self):
+        # Test, že vytvořený produkt má správnou cenu
+        product = Product.objects.get(name="Test product")
+        self.assertEqual(product.price, 10)
