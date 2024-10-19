cd08f3f5ea8f02be9298309ce968ba792f9a2b11
ssslipknottt <160328427+ssslipknottt@users.noreply.github.com>
2024-10-19 09:06:35 +0200

    feat: Test product a product.list funkcni

--- Carry_less/viewer/tests.py
+++ Carry_less/viewer/tests.py
@@ -6,9 +6,17 @@ from viewer.models import Product
 class ProductTest(TestCase):
     def setUp(self):
         # Vytvoření testovacího produktu
-        Product.objects.create(name="Test product", price=10)
+        Product.objects.create(name="Test product", price=10, weight=1, brand="Test brand", category_ID=1)
 
     def test_product(self):
         # Test, že vytvořený produkt má správnou cenu
         product = Product.objects.get(name="Test product")
         self.assertEqual(product.price, 10)
+
+    def test_product_list(self):
+        # Test, že vytvořený produkt vypise správnou cenu
+        product = Product.objects.get(name="Test product")
+        self.assertEqual(product.price, 10)
+
+
+
